68d14b3b16e3bfd378684e24bba69b4e75128112
Mhady Diallo <mhadysydney@gmail.com>
2025-05-20 22:18:51 +0000

    fixing curl error

--- service_runner.sh
+++ service_runner.sh
@@ -8,25 +8,60 @@ ICCID="8944538532057627725"
 TOKEN=""
 sms_to_send=""
 apiRetry=0
-
+SMS_STATE=0
 sendSMS(){
-   # local sms_to_send="$1"
-    echo $(date '+%Y-%m-%d %H:%M:%S') "Sending sms $sms_to_send" >> $LOG_FILE
-    TOKEN=$(cat auth_token.conf)
-    CURL_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
-                            --header 'Authorization: Bearer '$TOKEN \
+    #local send_status="$1"
+    if [ $SMS_STATE -eq 0 ];then
+        echo $(date '+%Y-%m-%d %H:%M:%S') "Sending sms $sms_to_send" >> $LOG_FILE
+        TOKEN=$(cat auth_token.conf)
+        echo $(date '+%Y-%m-%d %H:%M:%S') "Extracted Token: $TOKEN" >> $LOG_FILE
+        CURL_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
+                            --header "Authorization: Bearer $TOKEN" \
                             --header 'accept: application/json' \
                             --header 'content-type: application/*+json' \
                             --data '{"text":"'$sms_to_send'"}' \
-                        https://api.worldov.net/v1/sms/system/sim/$ICCID )
+                        https://api.worldov.net/v1/sms/system/sim/$ICCID 2>> $LOG_FILE)
+        echo $(date '+%Y-%m-%d %H:%M:%S') "SMS ($sms_to_send) Response: $CURL_RESPONSE, sms_state: $SMS_STATE)" >> $LOG_FILE
+        if [ "$CURL_RESPONSE" -eq 200 ]; then
+                echo $(date '+%Y-%m-%d %H:%M:%S') "SMS ($sms_to_send) sent Successfully to device (ICCID: $ICCID, Response: $CURL_RESPONSE, sms_state: $SMS_STATE)" >> $LOG_FILE
+                sleep 8
+                #sms_to_send=" 0224 cpureset"
+                #RESTART_FMB=$(sendSMS)
+                #sleep 10
+                RESTART_FMB=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
+                            --header "Authorization: Bearer $TOKEN" \
+                            --header 'accept: application/json' \
+                            --header 'content-type: application/*+json' \
+                           --data '{"text":" 0224 cpureset"}' \
+                        https://api.worldov.net/v1/sms/system/sim/$ICCID 2>> $LOG_FILE)
+                if [ "$RESTART_FMB" -eq 200 ]; then
+                    SMS_STATE=1
+                    apiRetry=0
+                    echo $(date '+%Y-%m-%d %H:%M:%S') "Device restarted successfully!" >> $LOG_FILE
+                else
+                   echo $(date '+%Y-%m-%d %H:%M:%S') "failed to send restart command with HTTP code $RESTART_FMB" >> $LOG_FILE
+                fi
+                #STATUS=$(echo $CURL_BODY | grep -o '"status":"[^"]*"' | cut -d'"' -f4)
+                #if [ "$STATUS" = "success" ]; then
+                #    echo "Successfully sent ngrok URL to FMB920 (ICCID: $ICCID, Response: $CURL_BODY)" >> $LOG_FILE
+                #else
+                 #   echo "Failed to send ngrok URL to FMB920 (ICCID: $ICCID, Response: $CURL_BODY)" >> $LOG_FILE
+                #fi
     
-    if [ "$CURL_RESPONSE" -eq 401 ]; then
-        sms_api_auth
-        apiRetry=$apiRetry+1
-        sleep 10
-        if [ $apiRetry<=5 ];then
-            sendSMS $sms_to_send
-
+        elif [ "$CURL_RESPONSE" -eq 401 ]; then
+            sms_api_auth
+            apiRetry=$apiRetry+1
+            sleep 10
+            if [ $apiRetry<=5 ];then
+                sendSMS $sms_to_send
+            else
+                echo $(date '+%Y-%m-%d %H:%M:%S') "sendSMS attempt limit reached exiting script service_runner, apiRetry:"$apiRetry >> $LOG_FILE
+                exit
+            fi
+        else
+                echo $(date '+%Y-%m-%d %H:%M:%S') "curl request failed with HTTP code $CURL_RESPONSE (SMS: $sms_to_send, sms_state: $SMS_STATE)" >> $LOG_FILE
+        fi
+    fi
     return $CURL_RESPONSE
 }
 
@@ -36,7 +71,7 @@ sms_api_auth(){
     password=$(grep "mdp=" fmb-control-app/auth.conf | cut -d'=' -f2)
     auth_response=$(curl -s -X POST \
             -H 'content-type: application/*+json' \
-            -d '{"username":"","password":""}'\
+            -d '{"username":"'$username'","password":"'$password'"}'\
             https://api.worldov.net/v1/auth/login
       )
     if [$auth_response -neq ""];then
@@ -50,7 +85,7 @@ sms_api_auth(){
 
 $PYTHON $FMB_DIR/tcp_server_v8.py >> $LOG_FILE 2>&1 &
 TCP_PID=$!
-SMS_STATE=0
+
 echo $(date '+%Y-%m-%d %H:%M:%S') "Started tcp_server_v8.py (PID: $TCP_PID)" >> $LOG_FILE
 
 $NGROK tcp 50122 --log $NGROK_LOG &
@@ -88,8 +123,10 @@ while true; do
             PORT=$(echo $NGROK_URL | cut -d':' -f3)
             echo $(date '+%Y-%m-%d %H:%M:%S') "Extracted ngrok URL: $NGROK_URL (Hostname: $HOSTNAME, Port: $PORT)" >> $LOG_FILE
             sms_to_send='" 0224 setparam 2004:'$HOSTNAME';2005:'$PORT'"'
-            CURL_RESPONSE=$(sendSMS $sms_to_send)
-            
+            CURL_RESPONSE=$(sendSMS)
+            #sleep 10
+            #sms_to_send=" 0224 cpureset"
+            #RESTART_FMB=$(sendSMS)
             # Send curl request to configure FMB920
             #CURL_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
             #                --header 'Authorization: Bearer '$TOKEN \
@@ -105,33 +142,7 @@ while true; do
               #  $API_URL 2>> $LOG_FILE)
 
             # Log curl response status
-            if [ "$CURL_RESPONSE" -eq 200 ]; then
-                SMS_STATE=1
-                echo $(date '+%Y-%m-%d %H:%M:%S') "Successfully sent ngrok URL to FMB920 (ICCID: $ICCID, Response: $CURL_RESPONSE, sms_state: $SMS_STATE)" >> $LOG_FILE
-                sleep 8
-                sms_to_send=" 0224 cpureset"
-                RESTART_FMB=$(sendSMS $sms_to_send)
-                sleep 10
-                #$(sendSMS $sms_to_send)$(curl -s -o /dev/null -w "%{http_code}" -X POST \
-                #            --header 'Authorization: Bearer '$TOKEN \
-                #            --header 'accept: application/json' \
-                #            --header 'content-type: application/*+json' \
-                #            --data '{"text":" 0224 cpureset"}' \
-                #        https://api.worldov.net/v1/sms/system/sim/$ICCID 2>> $LOG_FILE)
-                if [ "$RESTART_FMB" -eq 200 ]; then
-                    echo $(date '+%Y-%m-%d %H:%M:%S') "Device restarted successfully!" >> $LOG_FILE
-                else
-                    echo $(date '+%Y-%m-%d %H:%M:%S') "failed to send restart command" >> $LOG_FILE
-                fi
-                #STATUS=$(echo $CURL_BODY | grep -o '"status":"[^"]*"' | cut -d'"' -f4)
-                #if [ "$STATUS" = "success" ]; then
-                #    echo "Successfully sent ngrok URL to FMB920 (ICCID: $ICCID, Response: $CURL_BODY)" >> $LOG_FILE
-                #else
-                 #   echo "Failed to send ngrok URL to FMB920 (ICCID: $ICCID, Response: $CURL_BODY)" >> $LOG_FILE
-                #fi
-            else
-                echo $(date '+%Y-%m-%d %H:%M:%S') "curl request failed with HTTP code $CURL_RESPONSE (Response: $CURL_RESPONSE, sms_state: $SMS_STATE)" >> $LOG_FILE
-            fi
+            
     fi
     
     sleep 60
